a54028c03978a412727e5eb08f121d5296cd0f40
Matthew Wilson <wilson.503@wright.edu>
2018-08-30 14:00:08 -0400

    Added initial properties to world class

--- World.m
+++ World.m
@@ -2,10 +2,21 @@ classdef World
     %Class containg information and functions for simulating forest fires.
     
     properties
+        %Simulation Properties
         deltaT = 0.1 %Seconds
+        simulationTime = 0 %Time since simulation began
+        
+        %World information
+        tempratureCells
+        biomassCells
+        
+        worldTemprature
     end
     
     methods
+        function obj = World(Size)
+            obj.tempratureCells = obj.worldTemprature * ones(Size);
+        end
     end
     
 end
