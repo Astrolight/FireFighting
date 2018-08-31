bdb5587eaeeeb6c38c08a0e81eb192927ad974d3
Matthew Wilson <wilson.503@wright.edu>
2018-08-31 14:51:31 -0400

    Updated class to be by refrence rather then value

--- World.m
+++ World.m
@@ -1,4 +1,4 @@
-classdef World
+classdef World < handle
     %Class containg information and functions for simulating forest fires.
     
     properties
@@ -6,16 +6,42 @@ classdef World
         deltaT = 0.1 %Seconds
         simulationTime = 0 %Time since simulation began
         
-        %World information
-        tempratureCells
-        biomassCells
+        %World data storage cells
+        dataCells
+        %Structure to store data in
+        cellData=struct(...
+                'Temprature', [], ...
+                'Water_Level', [], ...
+                'Tree_Age', [], ...
+                'Biomass', [] ...
+                )
         
-        worldTemprature
+        %World information
+        worldTemprature = 20 %Deg c
+        worldSize
+        
+        %Plant proprties
+        typicalTreeAge = 150 %Years
     end
     
     methods
-        function obj = World(Size)
-            obj.tempratureCells = obj.worldTemprature * ones(Size);
+        function obj = World(World_Size)
+            if size(World_Size, 2) == 1
+                World_Size = [World_Size,World_Size];
+            end
+            
+            obj.worldSize = World_Size;
+            
+            obj.dataCells(obj.worldSize(:)) = obj.cellData;
+        end
+        
+        function obj = step(obj)
+            %Updates simulation time
+            obj.simulationTime = obj.simulationTime + obj.deltaT;
+        end
+        
+        function tempratureArray = getTempratureArray(obj)
+            tempratureArray = obj.dataCells{:,:}.Temprature
         end
     end
     
