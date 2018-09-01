208d73a6af9b5aa670848d10da461becd1387854
Matthew <Astrolight@users.noreply.github.com>
2018-08-31 22:31:10 -0400

    Changed uppercase 'Size' to lowercase 'size' for consistency

--- World.py
+++ World.py
@@ -2,17 +2,40 @@ import numba
 import numpy as np
 
 class World(object):
+    '''
+    Class to store the information of and control the simulation of a forest fire.
 
-    def __init__(self, Size):
+    Parameters
+    ----------
+    size: int or sequence of ints
+        Defines the size of the simulation in terms of how many subsections there are
+    '''
+
+    AVERAGETREEAGE = 150 # Years
+
+    def __init__(self, size):
         self.simTime = 0
         self.deltaTime = 0.1 # Seconds
 
-        if ~isinstance(Size, tuple):
-            # Sets up for Size to be the dims of a 2D matrix
-            Size = (Size,Size)
+        if ~isinstance(size, tuple):
+            # Sets up for size to be the dims of a 2D matrix
+            size = (size,size)
 
         # Matrixes contaning world data
-        self.tempratureMatrix = np.ones(Size)
+        self.tempratureMatrix = np.ones(size)
+        self.waterLevelMatrix = np.ones(size)
+        self.biomassMatrix = np.ones(size)
+        self.treeAgeMatrix = np.ones(size)
+
+    def getCurrentTemprature(self):
+        '''
+
+        '''
+        NotImplementedError
+
+    def chanceOfRain(self):
+        NotImplementedError
+
 
 # For debuging purposes
 if __name__=='__main__':
