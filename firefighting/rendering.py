3623894caa3d13305e8ba1da12b5a75d8de23bd2
Matthew <Astrolight@users.noreply.github.com>
2018-11-28 10:50:10 -0500

    Set up start of code to start rendering the heatmap

---
+++ firefighting/rendering.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def generateHeatmap(dataArray, minValue=0, maxValue=1):
+    '''
+    Takes in a numpy array and generates a heatmap
+
+    Parameters
+    ----------
+    dataArray: numpy array
+
+    minValue: float
+
+    maxValue: float
+
+    Returns
+    -------
+    figure object
+    '''
+    raise NotImplementedError()
