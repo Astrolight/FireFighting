7b9b72574098e9c8d13c77218d61abd139ca9fe4
Matthew <mathwilson45@hotmail.com>
2019-01-24 19:09:29 -0500

    Added file to start working on actual computation

---
+++ main.py
@@ -0,0 +1,18 @@
+import firefighting
+
+# Only run if this is the main file being ran
+if __name__ == '__main__':
+    world = firefighting.world.World(1000)
+    world.initRandomBiomass()
+
+    # Step by 100 days
+    for hour in range(24*100):
+        world.step()
+        
+    world_Data = world.getInfo()['worldData']
+
+    bioMass_Ammount = world_Data['BiomassAmount']
+
+    fig = firefighting.rendering.generateHeatmap(bioMass_Ammount, maxValue=14385)
+    
+    #fig.show()
