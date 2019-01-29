a6be8ab8c9433f42af0e6fa3a0bc59163e4b953e
Matthew <mathwilson45@hotmail.com>
2019-01-29 14:31:25 -0500

    Added timing for performance testing

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import firefighting
+import time
 
 # Only run if this is the main file being ran
 if __name__ == '__main__':
-    world = firefighting.world.World(100)
+
+    world = firefighting.world.World(500)
     world.initRandomBiomass()
 
+    start_time = time.time()
     # Step by 100 days
-    for hour in range(24*100):
+    for hour in range(24*365):
         world.step()
+    end_time = time.time()
+    total_time = end_time-start_time
+
+    print('Simulation took {:.2f} seconds for {} steps at {:.2f} steps per second.'.format(total_time, hour, hour/total_time))
         
     world_Data = world.getInfo()['worldData']
 
