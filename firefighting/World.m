c09761dc6f1982dbbac2e1321cb89a4a4f3681ac
Matthew <mathwilson45@hotmail.com>
2019-02-04 23:29:19 -0500

    Moved more python code over to MATLAB

--- firefighting/World.m
+++ firefighting/World.m
@@ -2,45 +2,48 @@ classdef World < handle
     %Class containg information and functions for simulating forest fires.
     
     properties
-        %Simulation Properties
-        deltaT = 0.1 %Seconds
-        simulationTime = 0 %Time since simulation began
+        simTime = 0;  % Hours
+        deltaTime = 24;  % Hours
+
+        % Sets the world size
+        fullWorldSize
+
+        % Creates a struct containg world elements
+        world
+
+        isOnFire = false;
+
+        % Veribles to make sure simulation is steped only when needed
+        lastSpreadTime = -100
+        lastSaveTime = -100
         
-        %Ver to store world data in
-        dataCells
-        
-        %World information
-        worldTemprature = 20 %Deg c
-        worldSize
+        % H5 file pointer
+        fp
     end
     
     methods
-        function obj = World(World_Size)
-            if size(World_Size, 2) == 1
-                World_Size = [World_Size,World_Size];
-            end
+        function obj = World(worldSize)
             
-            obj.worldSize = World_Size;
+            obj.fullWorldSize = [worldSize, worldSize];
             
-            obj.dataCells = cell(obj.worldSize);
-            obj.dataCells(:) = {TreeCell(5,5)};
-        end
-        
-        function obj = step(obj)
-            %Updates simulation time
-            obj.simulationTime = obj.simulationTime + obj.deltaT;
-        end
-        
-        function tempratureArray = getTempratureArray(obj)
-            tempratureArray = obj.dataCells{:,:}.Temprature;
+            obj.world = struct('WaterLevel', zeros(obj.fullWorldSize),...
+                      'BiomassAmount', zeros(obj.fullWorldSize),...
+                      'DiffTemprature', zeros(obj.fullWorldSize),...
+                      'treeAge', zeros(obj.fullWorldSize),...
+                      'hasFire', zeros(obj.fullWorldSize, 'logical'));
+            
+            % Stuff for creating h5 file
+            % Returns the current time in ISO 8601 UTC Format (yyyyMMddHHmmss)
+            current_time = char(datetime('now', 'Format', 'yyyyMMddHHmmSS'));
+
+            obj.create_h5(current_time);
         end
     end
     
-        
-    methods (Access=private, Static=true)
-        function biomassMap = generateInitBiomass(World_Size)
-            biomassMap = perlin2D(World_Size);
+    % Methods that should only be used by the class itself, not the user
+    methods (Access = protected)
+        function obj = create_h5(obj, filename)
+            obj.fp = H5F.create(filename);
         end
     end
-    
 end
