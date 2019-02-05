de7e341fe186750e9ed1b6f2dda0b87d59de4236
Matthew <mathwilson45@hotmail.com>
2019-02-04 23:37:50 -0500

    Fixed .h5 filename

--- firefighting/World.m
+++ firefighting/World.m
@@ -34,9 +34,26 @@ classdef World < handle
             
             % Stuff for creating h5 file
             % Returns the current time in ISO 8601 UTC Format (yyyyMMddHHmmss)
-            current_time = char(datetime('now', 'Format', 'yyyyMMddHHmmSS'));
+            current_time = strcat(char(datetime('now', 'Format', 'yyyyMMddHHmmSS')), '.h5');
 
             obj.create_h5(current_time);
+        end
+        
+        function obj = initRandomBiomass(obj)
+            % Generates a random ammount of biomass and age
+            randomAges = 120*365*24*np.random.random_sample(self.worldSize);
+
+            randomBiomass = 14385*np.random.random_sample(self.worldSize);
+
+            self.world['treeAge'] = randomAges;
+
+            self.world['BiomassAmount'] = biomass.growUp(...
+                randomBiomass, randomAges, 24 * self.deltaTime);
+        end    
+        
+        
+        function obj = step(obj)
+            
         end
     end
     
