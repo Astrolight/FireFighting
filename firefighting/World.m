89e89f9e3edbc0aae9daa84b4133ea76c606abd0
Matthew <mathwilson45@hotmail.com>
2019-02-05 20:12:44 -0500

    Finished adding saving code

--- firefighting/World.m
+++ firefighting/World.m
@@ -9,7 +9,7 @@ classdef World < handle
         fullWorldSize
 
         % Creates a struct containg world elements
-        world
+        world_data
 
         isOnFire = false;
 
@@ -23,8 +23,10 @@ classdef World < handle
         h5_file
         chunksize = 16
         
+        h5_frame_num = 1
+        
         % 1D data sets
-        h51D = {'simTime', 'onFire'};
+        h51D = {'simTime', 'isOnFire'};
         
         % 2D data sets
         h52D = {'WaterLevel', 'BiomassAmount', 'DiffTemprature', 'treeAge', 'hasFire'};
@@ -35,7 +37,7 @@ classdef World < handle
             
             obj.fullWorldSize = [worldSize, worldSize];
             
-            obj.world = struct('WaterLevel', zeros(obj.fullWorldSize),...
+            obj.world_data = struct('WaterLevel', zeros(obj.fullWorldSize),...
                       'BiomassAmount', zeros(obj.fullWorldSize),...
                       'DiffTemprature', zeros(obj.fullWorldSize),...
                       'treeAge', zeros(obj.fullWorldSize),...
@@ -48,30 +50,30 @@ classdef World < handle
             obj.create_h5(current_time);
         end
         
-        function obj = initRandomBiomass(obj)
+        function initRandomBiomass(obj)
             % Generates a random ammount of biomass and age
             randomAges = 120*365*24*rand(obj.fullWorldSize);
 
             randomBiomass = 14385*rand(obj.fullWorldSize);
 
-            obj.world.treeAge = randomAges;
+            obj.world_data.treeAge = randomAges;
 
-            obj.world.BiomassAmount = growUp(randomBiomass, randomAges, 24 * obj.deltaTime);
+            obj.world_data.BiomassAmount = growUp(randomBiomass, randomAges, 24 * obj.deltaTime);
         end    
         
         function Info = getInfo(obj)
             % Returns
             % -------
-            % out: dict
+            % out: struct
             %     Contains information on the current state of the simulation
 
             Info = struct('simTime', obj.simTime,...
                 'onFire', obj.isOnFire,...
                 'worldSize', obj.fullWorldSize,...
-                'worldData', obj.world);
+                'worldData', obj.world_data);
         end
         
-        function obj = step(obj)
+        function step(obj)
             
         end
         
@@ -83,6 +85,38 @@ classdef World < handle
             % Default is to save every simulation 720 steps and every 3 steps during a fire
             obj.normalInterval = normalInterval;
             obj.fireInterval = fireInterval;
+        end
+        
+        function saveState(obj)
+            % Saves the file in a hdf5 file format
+
+            % Disables warning about values being clampted when saved
+            warning('off', 'MATLAB:imagesci:hdf5dataset:datatypeOutOfRange')
+            
+            % Saves the 1d arrays
+            for dataset = obj.h51D
+                % For some reason always default to 1x1 cell
+                char_dataset = char(dataset);
+                
+                current_val = obj.(char_dataset);
+                converted_val = uint32(current_val);
+                
+                h5write(obj.h5_file, char(strcat('/', dataset)), converted_val, [1, obj.h5_frame_num], [1,1])
+            end
+
+            % Saves the 2d arrays
+            for dataset = obj.h52D
+                char_dataset = char(dataset);
+                
+                current_val = double(obj.world_data.(char_dataset));
+                
+                h5write(obj.h5_file, char(strcat('/world_data/', dataset)),...
+                    current_val, [1, 1, obj.h5_frame_num], [obj.fullWorldSize, 1])
+                
+            end
+            
+            % Bump up increment
+            obj.h5_frame_num = obj.h5_frame_num + 1;
         end
     end
     
@@ -96,7 +130,7 @@ classdef World < handle
             
             % Init 1D datasets
             h5create(obj.h5_file, '/simTime', [1, Inf], 'ChunkSize', [1, obj.chunksize], 'Datatype', 'uint32');
-            h5create(obj.h5_file, '/onFire', [1, Inf], 'ChunkSize', [1, obj.chunksize], 'Datatype', 'uint8');
+            h5create(obj.h5_file, '/isOnFire', [1, Inf], 'ChunkSize', [1, obj.chunksize], 'Datatype', 'uint8');
             
             % Init 2D datasets
             for dataset = obj.h52D
