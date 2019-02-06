ea55cd0a9dd82fa64d6f5770c7d8ad35fee288aa
Matthew <mathwilson45@hotmail.com>
2019-02-06 13:00:57 -0500

    Added progressbar

--- main.m
+++ main.m
@@ -1,14 +1,19 @@
 % To make sure all files are in the path
 setup
 
-world = World(1000);
+world = World(100);
 world.initRandomBiomass()
+
+f = waitbar(0, 'Running...');
 
 start_time = datetime('now');
 % Step by x days
-while world.simTime < 24*365
+sim_stop_time = 24*365*1000;
+while world.simTime <= sim_stop_time
     world.step();
+    waitbar(world.simTime/sim_stop_time, f)
 end
+close(f)
 end_time = datetime('now');
 total_time = end_time-start_time;
 
