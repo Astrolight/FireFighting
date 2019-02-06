05f62614b97dde1211b12dbc4720810439b1ed56
Matthew <mathwilson45@hotmail.com>
2019-02-05 20:37:24 -0500

    Updated total time calculation code

--- main.m
+++ main.m
@@ -4,12 +4,12 @@ setup
 world = World(1000);
 world.initRandomBiomass()
 
-start_time = now;
+start_time = datetime('now');
 % Step by x days
 while world.simTime < 24*365
     world.step();
 end
-end_time = now;
+end_time = datetime('now');
 total_time = end_time-start_time;
 
-sprintf('Simulation took %.2f seconds', total_time)
+disp(total_time)
