e65d68d990813551c7d811a1aa1d463c0ffe2860
Matthew <mathwilson45@hotmail.com>
2019-02-10 15:57:56 -0500

    Added biomass graphing over time

--- main.m
+++ main.m
@@ -18,3 +18,11 @@ end_time = datetime('now');
 total_time = end_time-start_time;
 
 disp(total_time)
+
+Datasets = dir('Datasets');
+last_dataset = Datasets(end);
+last_dataset = fullfile( last_dataset.folder, last_dataset.name);
+
+bio_history = h5read(last_dataset, '/world_data/BiomassAmount');
+
+implay(mat2gray(bio_history), 60);
