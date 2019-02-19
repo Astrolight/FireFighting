e9c7582642f6bb4780b35d68fc3d7c9ae356d5a7
Matthew <mathwilson45@hotmail.com>
2019-02-18 22:41:41 -0500

    I am sorry but my wait bar must go. I will miss it dearly

--- main.m
+++ main.m
@@ -4,16 +4,14 @@ setup
 world = World(100);
 world.initRandomBiomass()
 
-f = waitbar(0, 'Running...');
-
 start_time = datetime('now');
 % Step by x days
 sim_stop_time = 24*365*1000;
+
 while world.simTime <= sim_stop_time
     world.step();
-    waitbar(world.simTime/sim_stop_time, f)
 end
-close(f)
+
 end_time = datetime('now');
 total_time = end_time-start_time;
 
