ae6396a19c785e5869c6c5c842d56484470f284e
Matthew <mathwilson45@hotmail.com>
2019-04-24 15:23:00 -0400

    Added graphs for data

--- analysisData.m
+++ analysisData.m
@@ -8,7 +8,7 @@ Datasets = Datasets(3:end);
 
 % Dataset, subdataset, Fire, (Edge, Mass)
 % Hopping only 500 fires max
-processed_data = zeros(length(Datasets), 10, 500, 2);
+processed_data = NaN(length(Datasets), 10, 500, 2);
 
 for i = 1:length(Datasets)
     folder = strcat('Datasets','/',Datasets(i).name);
@@ -27,4 +27,24 @@ for i = 1:length(Datasets)
             processed_data(i,n,k,2) = test(k).total_burned_biomass;
         end
     end 
+end
+
+for i = 1:size(processed_data,1)
+    for n=1:2
+        new_processed_data(i,:,n) = reshape(processed_data(i,:,:,n),1,[]);
+    end
+end
+
+for i = 1:size(processed_data,1)
+    fig = figure;
+    scatter(new_processed_data(i,:,2),new_processed_data(i,:,1))
+    title(Datasets(i).name)
+    xlabel('Mass Burned in Fire')
+    ylabel('Fraction of Edges Burned')
+    xlim([0,3E5])
+    ylim([0,1])
+    hold on
+    plot(nanmean(new_processed_data(i,:,2)),nanmean(new_processed_data(i,:,1)),'r.','MarkerSize', 25)
+    hold off
+    saveas(fig, strcat(Datasets(i).name,'.png'))
 end
